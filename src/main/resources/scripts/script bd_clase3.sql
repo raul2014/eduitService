29daa5df0337b08a023ce5de83a4c5db323e1117
raul_jorge_coronado <raul_jorge_coronado@yahoo.com.ar>
2019-01-12 12:06:50 -0300

    Subida de script de bd

---
+++ src/main/resources/scripts/script bd_clase3.sql
@@ -0,0 +1,10 @@
+create schema if not exists autos;
+
+use autos;
+create table if not exists cars (
+	id int(11) not null auto_increment,
+    brand varchar(255) null default '',
+    model varchar(255) null default '',
+    price double,
+    primary key (id)
+)
